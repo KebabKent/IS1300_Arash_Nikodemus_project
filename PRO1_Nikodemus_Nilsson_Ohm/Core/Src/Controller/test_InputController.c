61d6830f75de0b5443a4308dbd11e4c0ccd64d0f
kebabkent <nikodemus.ohm2@gmail.com>
2026-01-30 15:41:04 +0100

    help

--- PRO1_Nikodemus_Nilsson_Ohm/Core/Src/Controller/test_InputController.c
+++ PRO1_Nikodemus_Nilsson_Ohm/Core/Src/Controller/test_InputController.c
@@ -1,14 +1,34 @@
 #include "Controller/test_InputController.h"
 #include "Controller/InputController.h"
+#include "stdint.h"
 
 void Test_Input(void) {
-
+	test_readMomentaryButton();
+	test_readToggleSwitch();
+	test_readAndSetInputsState();
 }
 
+// readMomentaryButton(GPIO_TypeDef* Port, uint16_t Pin, uint8_t* lastState, bool* outFlag)
 void test_readMomentaryButton() {
+	InputState_t state;
+
+
+	uint32_t startTime = HAL_GetTick();
+	const uint32_t testDuration = 10000;
+
+
+	while ((HAL_GetTick() - startTime) < testDuration) {
+
+		HAL_Delay(100);
+	}
+
+
+    readMomentaryButton(PL1_Switch_GPIO_Port, PL1_Switch_Pin, &lastBtnLeft, &state->Button_Pressed_Left);
+    readMomentaryButton(PL2_Switch_GPIO_Port, PL2_Switch_Pin, &lastBtnUp,   &state->Button_Pressed_Up);
 
 }
 
+// void readToggleSwitch(GPIO_TypeDef* Port, uint16_t Pin, bool* outState)
 void test_readToggleSwitch() {
 
 }
