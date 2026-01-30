7be3626fd2171914cfd26173696715514df02521
kebabkent <nikodemus.ohm2@gmail.com>
2026-01-30 16:00:20 +0100

    stuff

--- PRO1_Nikodemus_Nilsson_Ohm/Core/Src/Controller/test_InputController.c
+++ PRO1_Nikodemus_Nilsson_Ohm/Core/Src/Controller/test_InputController.c
@@ -1,6 +1,10 @@
 #include "Controller/test_InputController.h"
 #include "Controller/InputController.h"
 #include "stdint.h"
+#include "stdbool.h"
+#include "spi.h"
+#include "main.h"
+#include "Model/traffic_state.h"
 
 void Test_Input(void) {
 	test_readMomentaryButton();
@@ -10,21 +14,32 @@ void Test_Input(void) {
 
 // readMomentaryButton(GPIO_TypeDef* Port, uint16_t Pin, uint8_t* lastState, bool* outFlag)
 void test_readMomentaryButton() {
-	InputState_t state;
+	static uint8_t lastBtnLeft = GPIO_PIN_SET;
+	static uint8_t lastBtnUp   = GPIO_PIN_SET;
 
+	bool btnLeft = false;
+	bool btnUp = false;
 
 	uint32_t startTime = HAL_GetTick();
 	const uint32_t testDuration = 10000;
 
 
 	while ((HAL_GetTick() - startTime) < testDuration) {
-
+		readMomentaryButton(PL1_Switch_GPIO_Port, PL1_Switch_Pin, &lastBtnLeft, &btnLeft);
+		readMomentaryButton(PL2_Switch_GPIO_Port, PL2_Switch_Pin, &lastBtnUp, &btnUp);
 		HAL_Delay(100);
 	}
 
+	uint8_t data[3] = {0,0,0};
 
-    readMomentaryButton(PL1_Switch_GPIO_Port, PL1_Switch_Pin, &lastBtnLeft, &state->Button_Pressed_Left);
-    readMomentaryButton(PL2_Switch_GPIO_Port, PL2_Switch_Pin, &lastBtnUp,   &state->Button_Pressed_Up);
+	if (btnLeft) data[2] = 0x8;
+	if (btnLeft) data[1] = 0x800;
+
+
+	HAL_GPIO_WritePin(SR_STCP_GPIO_Port, SR_STCP_Pin, GPIO_PIN_RESET);
+	HAL_SPI_Transmit(&hspi3, data, 3, 100);
+	HAL_GPIO_WritePin(SR_STCP_GPIO_Port, SR_STCP_Pin, GPIO_PIN_SET);
+
 
 }
 
